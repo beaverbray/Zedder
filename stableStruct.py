62ce20dc5af3a8398b94aafc009a3c4ab63e8191
beaverbray <bakejray@gmail.com>
2021-06-09 13:35:44 -0700

    updated strut

--- stableStruct.py
+++ stableStruct.py
@@ -17,25 +17,64 @@ from pprint import pprint
 my_dict = query()
 
 # parse json until we get to strs
-getRaceResults = (my_dict['data']['getRaceResults']['edges'])
+fdsa = my_dict['data']['getRaceResults']['edges']
+
+# getRaceResults = (my_dict['data']['getRaceResults']['edges'][0]['node'])
 # transform strs to dict
 # print(getRaceResults)
 
-# print(type(getRaceResults))
-edges = dict()
+# print(len(fdsa))
 
-for index, value in enumerate(getRaceResults):
- 	edges[index] = value
+d = []
+for n in range(0, len(fdsa)):
+	 d.append((my_dict['data']['getRaceResults']['edges'][n]['node']))
 
-def myprint(race):
-	# key and value in race(items)
-    for k, v in race.items():
-    	# isinstace returns True if V in DICT
-        if isinstance(v, dict):
-            myprint(v)
-        else:
-        	# prints key and values
-            pprint("{0} : {1}".format(k, v))
+
+pprint(d[0])
+pprint(d[1])
+
+# def raceParse():
+
+# 	for race in d:
+# 		for v in race:
+# 			print()
+
+
+# 	print(city)
+
+
+# raceParse()
+# prize pool calc
+# # wei * 0.000000000000000001 = eth
+
+# # 'city': 'Manila',
+# #               'class': 1,
+# #               'country': 'Philippines',
+# #               'fee': '0.0149',
+
+# # def raceDescription():
+
+# # 	data = myprint(edges)
+	
+# # 	city = data['node']['city']
+	
+# # 	pprint(data)
+# # 	# print(f'we are racing in{city} today')
+
+# # 	# data = myprint(edges)
+# # 	# pprint(data)
+
+
+# # raceDescription()
+
+
+
+
+
+
+
+
+
 
 
 
